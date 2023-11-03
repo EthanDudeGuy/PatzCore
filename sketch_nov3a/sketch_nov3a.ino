d2b278dc9aa7b1cde7768e0ca8878259cf713b9e
EthanDudeGuy <ethan.emery01@gmail.com>
2023-11-03 11:24:37 -0700

    test code push

---
+++ sketch_nov3a/sketch_nov3a.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, LOW);
+  digitalWrite(3, HIGH);
+  delayMicroseconds(60);
+
+}
